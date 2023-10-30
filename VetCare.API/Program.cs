e8076f091e96348a925246b587c68085d751f52f
Yorusito <sebastian1987102@gmail.com>
2023-10-29 23:02:21 -0500

    initial commit

---
+++ VetCare.API/Program.cs
@@ -0,0 +1,82 @@
+using VetCare.API.Appointment.Domain.Repositories;
+using VetCare.API.Appointment.Domain.Services;
+using VetCare.API.Appointment.Mapping;
+using VetCare.API.Appointment.Persistence.Repositories;
+using VetCare.API.Appointment.Services;
+using VetCare.API.Store.Domain.Repositories;
+using VetCare.API.Store.Domain.Services;
+using VetCare.API.Store.Mapping;
+using VetCare.API.Store.Persistence.Repositories;
+using VetCare.API.Store.Services;
+using VetCare.API.Shared.Persistence.Contexts;
+using VetCare.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Database Connection
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+// Add lowercase routes
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Dependency Injection Configuration
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUnitOfWorkS, UnitOfWorkS>();
+builder.Services.AddScoped<IPetRepository, PetRepository>();
+builder.Services.AddScoped<IPetService, PetService>();
+builder.Services.AddScoped<IPrescriptionRepository, PrescriptionRepository>();
+builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
+// AutoMapper Configuration
+
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProduct), 
+    typeof(ResourceToModelProduct), 
+    typeof(ModelToResourceProfile), 
+    typeof(ResourceToModelProfile));
+    
+
+var app = builder.Build();
+
+// Validation for ensuring Database Objects are created
+
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
