0873fbbf3180af5b3c4378a0824afb2572c134ee
daniel <u202121440@upc.edu.pe>
2023-10-30 00:16:44 -0500

    feat(store): added product mapping

---
+++ VetCare.API/Store/Mapping/ModelToResourceProduct.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using VetCare.API.Store.Domain.Models;
+using VetCare.API.Store.Resources;
+
+namespace VetCare.API.Store.Mapping;
+
+public class ModelToResourceProduct : Profile
+{
+    public ModelToResourceProduct()
+    {
+        CreateMap<Product, ProductResource>();
+    }
+}
