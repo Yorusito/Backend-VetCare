862d47d4b5776fd5d5803c43952bafcb14edd99c
JulianaYS <u20201b782@upc.edu.pe>
2023-11-01 22:34:57 -0500

    feat(identification): Added mapping between register request and update request with user.

---
+++ VetCare.API/Identification/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using VetCare.API.Identification.Domain.Models;
+using VetCare.API.Identification.Domain.Services.Communication;
+using VetCare.API.Identification.Resources;
+
+namespace LearningCenter.API.Identification.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<User, AuthenticateResponse>();
+        CreateMap<User, UserResource>();
+    }
+}
