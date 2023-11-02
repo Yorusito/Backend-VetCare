24125a8548269a374f20209816bf85fd6ad3a558
JulianaYS <u20201b782@upc.edu.pe>
2023-11-02 01:12:33 -0500

    feat(identification): Implemented user service methods.

--- VetCare.API/Identification/Services/UserService.cs
+++ VetCare.API/Identification/Services/UserService.cs
@@ -4,6 +4,8 @@ using VetCare.API.Identification.Domain.Models;
 using VetCare.API.Identification.Domain.Repositories;
 using VetCare.API.Identification.Domain.Services;
 using VetCare.API.Identification.Domain.Services.Communication;
+using VetCare.API.Identification.Exceptions;
+using BCryptNet = BCrypt.Net.BCrypt;
 
 namespace VetCare.API.Identification.Services;
 
@@ -25,28 +27,80 @@ public class UserService : IUserService
         throw new NotImplementedException();
     }
 
-    public Task<IEnumerable<User>> ListAsync()
+    public async Task<IEnumerable<User>> ListAsync()
     {
-        throw new NotImplementedException();
+        return await _userRepository.ListAsync();
     }
 
-    public Task<User> GetByIdAsync(int id)
+    public async Task<User> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = await _userRepository.FindByIdAsync(id);
+        if (user == null) throw new KeyNotFoundException("User not found");
+        return user;
     }
 
-    public Task RegisterAsync(RegisterRequest model)
+    public async Task RegisterAsync(RegisterRequest request)
     {
-        throw new NotImplementedException();
+        // validate
+        if (_userRepository.ExistsByEmail(request.Email)) 
+            throw new AppException("Email '" + request.Email + "' is already taken");
+        // map model to new user object
+        var user = _mapper.Map<User>(request);
+        // hash password
+        user.PasswordHash = BCryptNet.HashPassword(request.Password);
+        // save user
+        try
+        {
+            await _userRepository.AddAsync(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while saving the user: {e.Message}");
+        }
     }
 
-    public Task UpdateAsync(int id, UpdateRequest model)
+    public async Task UpdateAsync(int id, UpdateRequest request)
     {
-        throw new NotImplementedException();
+        var user = GetById(id);
+        // Validate
+        if (_userRepository.ExistsByEmail(request.Email)) 
+            throw new AppException("Username '" + request.Email + "' is already taken");
+        // Hash password if it was entered
+        if (!string.IsNullOrEmpty(request.Password))
+            user.PasswordHash = BCryptNet.HashPassword(request.Password);
+        // Copy model to user and save
+        _mapper.Map(request, user);
+        try
+        {
+            _userRepository.Update(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while updating the user: {e.Message}");
+        }
     }
 
-    public Task DeleteAsync(int id)
+    public async Task DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        var user = GetById(id);
+        try
+        {
+            _userRepository.Remove(user);
+            await _unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new AppException($"An error occurred while deleting the user: {e.Message}");
+        }
+
+    }
+    
+    private User GetById(int id)
+    {
+        var user = _userRepository.FindById(id);
+        if (user == null) throw new KeyNotFoundException("User not found");
+        return user;
     }
 }
