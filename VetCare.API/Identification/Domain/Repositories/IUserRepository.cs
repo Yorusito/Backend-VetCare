85b6ef6a39c361c27b916b84ab2ba73994861dc4
JulianaYS <u20201b782@upc.edu.pe>
2023-11-02 00:13:43 -0500

    feat(identification): Implemented user table.

--- VetCare.API/Identification/Domain/Repositories/IUserRepository.cs
+++ VetCare.API/Identification/Domain/Repositories/IUserRepository.cs
@@ -7,8 +7,8 @@ public interface IUserRepository
     Task<IEnumerable<User>> ListAsync();
     Task AddAsync(User user);
     Task<User> FindByIdAsync(int id);
-    Task<User> FindByUsernameAsync(string username);
-    public bool ExistsByUsername(string username);
+    Task<User> FindByUsernameAsync(string email);
+    public bool ExistsByUsername(string email);
     User FindById(int id);
     void Update(User user);
     void Remove(User user);
