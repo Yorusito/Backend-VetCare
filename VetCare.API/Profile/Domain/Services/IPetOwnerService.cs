22f0c9c2264ce208431358b58ebcfd382c85f18d
JulianaYS <u20201b782@upc.edu.pe>
2023-11-21 01:26:41 -0500

    feat(profile): added pet owner resource and implemented interface.

---
+++ VetCare.API/Profile/Domain/Services/IPetOwnerService.cs
@@ -0,0 +1,12 @@
+﻿using VetCare.API.Profile.Domain.Models;
+using VetCare.API.Profile.Domain.Services.Communication;
+
+namespace VetCare.API.Profile.Domain.Services;
+
+public interface IPetOwnerService
+{
+    Task<IEnumerable<PetOwner>> ListAsync();
+    Task<PetOwnerResponse> SaveAsync(PetOwner petOwner);
+    Task<PetOwnerResponse> UpdateAsync(int id, PetOwner petOwner);
+    Task<PetOwnerResponse> DeleteAsync(int id);
+}
