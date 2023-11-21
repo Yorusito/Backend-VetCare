0025afd337c112a20e9834ab9d625a1771aab2d8
JulianaYS <u20201b782@upc.edu.pe>
2023-11-21 01:16:54 -0500

    feat(profile): added pet owner response.

---
+++ VetCare.API/Profile/Domain/Services/Communication/PetOwnerResponse.cs
@@ -0,0 +1,15 @@
+﻿using VetCare.API.Profile.Domain.Models;
+using VetCare.API.Shared.Domain.Services.Communication;
+
+namespace VetCare.API.Profile.Domain.Services.Communication;
+
+public class PetOwnerResponse : BaseResponse<PetOwner>
+{
+    public PetOwnerResponse(string message) : base(message)
+    {
+    }
+
+    public PetOwnerResponse(PetOwner resource) : base(resource)
+    {
+    }
+}
